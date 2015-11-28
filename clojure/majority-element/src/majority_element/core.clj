700bd835de8b81e1009a278dd77cbdfe330ecda4
David Hofer <david.hofer@gmail.com>
2015-11-27 21:49:51 -0500

    Better name.

--- clojure/majority-element/src/majority_element/core.clj
+++ clojure/majority-element/src/majority_element/core.clj
@@ -9,7 +9,7 @@
   (map read-string (str/split (slurp filename) #","))
   )
 
-(defn add-or-increment
+(defn insert-or-increment
   "Given a map and an element, looks up the element in the map.  If the element isn't present, adds it with a value of 1, otherwise increments the existing value."
   [element-map element]
   (if (get element-map element)
@@ -21,7 +21,7 @@
 (defn majority-element
   "Given an input list of integers, returns either the one that occurs greater than 50% of the time in the list if it exists, otherwise nil."
   [elements]
-  (def count-map (reduce add-or-increment {} elements))
+  (def count-map (reduce insert-or-increment {} elements))
   (def result (filter (fn [[k v]] (> v (/ (count elements) 2))) count-map))
   (first (keys result))
   )
